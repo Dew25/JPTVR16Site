40eb4a23d49c345b616e066254a10c99a863f4de
Juri Melnikov <Juri.Melnikov@ivkhk.ee>
2019-03-26 15:27:21 +0200

    first commit

---
+++ src/java/entity/UserArticles.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author Melnikov
+ */
+@Entity
+public class UserArticles implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    private User user;
+    @OneToOne
+    private Article article;
+    
+    
+}
